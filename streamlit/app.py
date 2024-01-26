672c11177c9bd5849828fcd9726323cdcd67e2ec
pulpo125 <kang.pulpo@gmail.com>
2024-01-26 22:20:45 +0900

    heatmap 에러 해결

--- streamlit/app.py
+++ streamlit/app.py
@@ -35,11 +35,9 @@ col3.markdown("##### Stock price Graph")
 col3.line_chart(stocks_df.iloc[:,1::])
 
 # correlation plot
-col4.write("##### Correlation Analysis")
-try:
-    col4.dataframe(stocks_df.corr(method = 'pearson', numeric_only=True), width=340)
-except:
-    print("error")
+col4.write("##### Correlation Analysis Heatmap")
+plot = sns.heatmap(stocks_df.corr(method = 'pearson', numeric_only=True), cmap='YlOrRd', annot=True)
+col4.pyplot(plot.get_figure())
 
 st.markdown("##### :pushpin: 상관관계 분석 결과")
 st.markdown('''
@@ -81,11 +79,9 @@ col7.markdown("##### Stock price Graph")
 col7.line_chart(stocks_3_df.iloc[:,1::])
 
 # correlation plot
-col8.write("##### Correlation Analysis")
-try:
-    col8.dataframe(stocks_3_df.corr(method = 'pearson', numeric_only=True), width=340)
-except:
-    print("error")
+col4.write("##### Correlation Analysis Heatmap")
+plot = sns.heatmap(stocks_3_df.corr(method = 'pearson', numeric_only=True), cmap='YlOrRd', annot=True)
+col4.pyplot(plot.get_figure())
 
 st.markdown("##### :pushpin: 상관관계 분석 결과")
 st.markdown('''
